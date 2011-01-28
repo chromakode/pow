6ad5434589c6eb5a9bb63d9de3b6b951294e987b
chromakode <c@chromakode.com>
2011-01-28 01:44:15 -0800

    Animations, slide objects, and slide scripts, oh my!

--- pow.js
+++ pow.js
@@ -1,6 +1,6 @@
 // {POW!}
 
-(function pow() {
+(function() {
 	pow = {}
 
 	pow.log = function() {
@@ -53,6 +53,16 @@
 				handlers[i].apply(null, arguments)
 			}
 		}
+		register.remove = function(handler) {
+			handlers.splice(handlers.indexOf(handler), 1)
+		}
+		register.once = function(handler) {
+			register(function() {
+				handler.apply(null, arguments)
+				register.remove(handler)
+			})
+		}
+		register.handlers = handlers
 		return register
 	}
 
@@ -60,26 +70,85 @@
 	pow.on.load = new pow.signal()
 	// pow.on.cleanup -- for cleaning the dom prior to saves?
 
-	pow.slides = []
-	pow.slides.show = function(slide) {
-		slide = slide || pow.slides[0]
-		if (pow.slides.current) {
-			pow.slides.current.classList.remove('current')
+	pow.animate = function(callback, duration) {
+		var elapsed, duration, interval
+		function frame() {
+			callback(elapsed/duration)
+			elapsed += step
+			if (duration && elapsed > duration) {
+				stop()
+			}
 		}
-		slide.classList.add('current')
-		pow.slides.current = slide
+		function stop() {
+			if (interval) {
+				callback(1)
+				clearInterval(interval)
+				interval = null
+			}
+		}
+		elapsed = 0
+		step = 1000/60
+		interval = setInterval(frame, step)
+		return stop
 	}
-	pow.slides.next = function() {
-		pow.slides.show(pow.slides.current.nextElementSibling || pow.slides[0])
+
+	// Placeholder API during DOM load for scripting convenience.
+	// This gets replaced with the current slide as soon as we have one.
+	pow.slide = {}
+	pow.slide.hide = function() {}
+	pow.slide.on = {}
+	pow.slide.on.show = function() {}
+
+	pow.Slide = function(index, el) {
+		this.index = index
+		this.el = el
+		this._setup();
 	}
-	pow.slides.previous = function() {
-		pow.slides.show(pow.slides.current.prevElementSibling || pow.slides[pow.slides.length])
+	pow.Slide.prototype = {
+		_setup: function() {
+			this.on = {}
+			this.on.show = new pow.signal()
+			this.on.hide = new pow.signal()
+
+			var prevSlide = pow.slide
+			pow.slide = this
+			var scripts = this.el.getElementsByTagName('script')
+			Array.prototype.forEach.call(scripts, function(script) {
+				if (/^\s*pow\.slide\.on\.\w*\([\s\S]*\)\s*$/.test(script.innerHTML)) {
+					eval(script.innerHTML)
+				}
+			})
+			pow.slide = prevSlide
+		},
+		show: function() {
+			pow.slide.hide()
+			this.el.classList.add('current')
+			pow.slide = this
+			this.on.show.fire()
+		},
+		hide: function() {
+			this.el.classList.remove('current')
+			this.on.hide.fire()
+		},
+		get next() {
+			return (this.index < pow.slides.length - 1) && pow.slides[this.index + 1]
+		},
+		get previous() {
+			return (this.index > 0) && pow.slides[this.index - 1]
+		},
+		animate: function(callback, duration) {
+			var stop = pow.animate(callback, duration)
+			this.on.hide.once(stop)
+		}
 	}
+
+	pow.slides = []
 	pow.slides.load = pow.on.load(function() {
-		Array.prototype.forEach.call(document.getElementsByClassName('slide'), function(slideEl) {
-			pow.slides.push(slideEl)
+		var els = document.getElementsByClassName('slide')
+		Array.prototype.forEach.call(els, function(el, index) {
+			pow.slides.push(new pow.Slide(index, el))
 		})
-		pow.slides.show()
+		pow.slides[0].show()
 	})
 
 	pow.slides.style = {}
@@ -112,7 +181,7 @@
 	window.addEventListener('load', pow.on.load.fire, false)
 	window.addEventListener('click', function(e) {
 		if (!e.target.isContentEditable) {
-			pow.slides.next()
+			if (pow.slide.next) { pow.slide.next.show() }
 			e.preventDefault()
 		}
 	}, false)
