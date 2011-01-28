92fc09724fc8db63ca78337081535bd3c5328e12
chromakode <c@chromakode.com>
2011-01-27 23:54:41 -0800

    Now with inf% more correct updating!

--- pow.js
+++ pow.js
@@ -1,24 +1,29 @@
 // {POW!}
 
-pow = {}
+(function pow() {
+	pow = {}
 
-pow.log = function() {
-	if (console.log && pow.log.enabled) {
-		console.log.apply(console, arguments)
+	pow.log = function() {
+		if (console.log && pow.log.enabled) {
+			console.log.apply(console, arguments)
+		}
 	}
-}
-pow.log.enabled = true
+	pow.log.enabled = true
 
-;(pow.loader = function() {
-	pow.log('{POW!}')
-	var powScript = document.getElementById('pow')
-	if (!powScript.hasAttribute('data-loaded')) {
-		// TODO: Replace this double request silliness with a string eval once we have a compilation system in place.
-		try {
-			var req = new XMLHttpRequest(),
-				origin = powScript.src || powScript.getAttribute('data-origin')
-			req.open('GET', origin, false)
-			req.send()
+	pow.update = function() {
+		pow.log('{POW!}')
+		var powScript = document.getElementById('pow')
+		if (!powScript.hasAttribute('data-loaded')) {
+			// TODO: Replace this double request silliness with a string eval once we have a compilation system in place.
+			try {
+				var req = new XMLHttpRequest(),
+					origin = powScript.src || powScript.getAttribute('data-origin')
+				req.open('GET', origin, false)
+				req.send()
+			} catch (err) {
+				pow.log('Failed to update pow.js from ' + origin + '. Continuing.')
+				return
+			}
 			if (req.status == 200) {
 				var innerScript = document.createElement('script')
 				innerScript.id = 'pow'
@@ -28,87 +33,87 @@ pow.log.enabled = true
 				pow.log('Loaded updated pow.js from ' + origin + '. Restarting.')
 				document.head.insertBefore(innerScript, powScript)
 				document.head.removeChild(powScript)
-				return
+				return true
 			}
-		} catch (err) {}
-		pow.log('Failed to update pow.js from ' + origin + '. Continuing.')
-	} else {
-		powScript.removeAttribute('data-loaded')
-		pow.log('Detected restart. Continuing.')
-	}
-})()
-
-pow.signal = function() {
-	var handlers = []
-	function register(handler) {
-		handlers.push(handler)
-		return handler
-	}
-	register.fire = function() {
-		for (var i = 0; i < handlers.length; i++) {
-			handlers[i].apply(null, arguments)
+		} else {
+			powScript.removeAttribute('data-loaded')
+			pow.log('Detected restart. Continuing.')
 		}
 	}
-	return register
-}
+	if (pow.update() == true) { return }
 
-pow.on = {}
-pow.on.load = new pow.signal()
-// pow.on.cleanup -- for cleaning the dom prior to saves?
-
-pow.slides = []
-pow.slides.show = function(slide) {
-	slide = slide || pow.slides[0]
-	if (pow.slides.current) {
-		pow.slides.current.classList.remove('current')
+	pow.signal = function() {
+		var handlers = []
+		function register(handler) {
+			handlers.push(handler)
+			return handler
+		}
+		register.fire = function() {
+			for (var i = 0; i < handlers.length; i++) {
+				handlers[i].apply(null, arguments)
+			}
+		}
+		return register
 	}
-	slide.classList.add('current')
-	pow.slides.current = slide
-}
-pow.slides.next = function() {
-	pow.slides.show(pow.slides.current.nextElementSibling || pow.slides[0])
-}
-pow.slides.previous = function() {
-	pow.slides.show(pow.slides.current.prevElementSibling || pow.slides[pow.slides.length])
-}
-pow.slides.load = pow.on.load(function() {
-	Array.prototype.forEach.call(document.getElementsByClassName('slide'), function(slideEl) {
-		pow.slides.push(slideEl)
+
+	pow.on = {}
+	pow.on.load = new pow.signal()
+	// pow.on.cleanup -- for cleaning the dom prior to saves?
+
+	pow.slides = []
+	pow.slides.show = function(slide) {
+		slide = slide || pow.slides[0]
+		if (pow.slides.current) {
+			pow.slides.current.classList.remove('current')
+		}
+		slide.classList.add('current')
+		pow.slides.current = slide
+	}
+	pow.slides.next = function() {
+		pow.slides.show(pow.slides.current.nextElementSibling || pow.slides[0])
+	}
+	pow.slides.previous = function() {
+		pow.slides.show(pow.slides.current.prevElementSibling || pow.slides[pow.slides.length])
+	}
+	pow.slides.load = pow.on.load(function() {
+		Array.prototype.forEach.call(document.getElementsByClassName('slide'), function(slideEl) {
+			pow.slides.push(slideEl)
+		})
+		pow.slides.show()
 	})
-	pow.slides.show()
-})
 
-pow.slides.style = {}
-pow.slides.style.resize = function() {
-	if (!this.el) {
-		this.el = document.createElement('style')
-		this.el.id = 'pow-slide-style'
-		document.head.appendChild(this.el)
+	pow.slides.style = {}
+	pow.slides.style.resize = function() {
+		if (!this.el) {
+			this.el = document.createElement('style')
+			this.el.id = 'pow-slide-style'
+			document.head.appendChild(this.el)
+		}
+		var slides = document.getElementById('slides'),
+			width = Math.min(slides.offsetWidth, (4/3) * slides.offsetHeight),
+			height = .75 * width,
+			padLeft = (slides.offsetWidth - width) / 2
+			padTop = (slides.offsetHeight - height) / 2
+			size = width / 800
+		this.el.innerHTML =
+			'.slide {'
+				+ ' left:'+padLeft+'px;'
+				+ ' top:'+padTop+'px;'
+				+ ' height:'+height+'px;'
+				+ ' width:'+width+'px;'
+			+' }\n'
+			+'#slides { font-size:'+size+'px; }'
 	}
-	var slides = document.getElementById('slides'),
-		width = Math.min(slides.offsetWidth, (4/3) * slides.offsetHeight),
-		height = .75 * width,
-		padLeft = (slides.offsetWidth - width) / 2
-		padTop = (slides.offsetHeight - height) / 2
-		size = width / 800
-	this.el.innerHTML =
-		'.slide {'
-			+ ' left:'+padLeft+'px;'
-			+ ' top:'+padTop+'px;'
-			+ ' height:'+height+'px;'
-			+ ' width:'+width+'px;'
-		+' }\n'
-		+'#slides { font-size:'+size+'px; }'
-}
-pow.slides.style.load = pow.on.load(function() {
-	pow.slides.style.resize()
-	window.addEventListener('resize', function() { pow.slides.style.resize() }, false)
-})
+	pow.slides.style.load = pow.on.load(function() {
+		pow.slides.style.resize()
+		window.addEventListener('resize', function() { pow.slides.style.resize() }, false)
+	})
 
-window.addEventListener('load', pow.on.load.fire, false)
-window.addEventListener('click', function(e) {
-	if (!e.target.isContentEditable) {
-		pow.slides.next()
-		e.preventDefault()
-	}
-}, false)
+	window.addEventListener('load', pow.on.load.fire, false)
+	window.addEventListener('click', function(e) {
+		if (!e.target.isContentEditable) {
+			pow.slides.next()
+			e.preventDefault()
+		}
+	}, false)
+})()
