9112c83c9a58ce12e389efa00300b41a63719a54
chromakode <c@chromakode.com>
2011-03-14 00:39:07 -0700

    Add fallback for document.head for older versions of Firefox.

--- pow.core.js
+++ pow.core.js
@@ -1,4 +1,6 @@
 pow.module('core', function() {
+	document.head || (document.head = document.getElementsByTagName('head')[0]);
+
 	pow.signal = function() {
 		var handlers = []
 		function register(handler) {
