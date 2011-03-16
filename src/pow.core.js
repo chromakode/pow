d6cae012aa2e76ddff6e116c8b6a2742a121220a
chromakode <c@chromakode.com>
2011-03-15 22:31:33 -0700

    Comment update.

--- src/pow.core.js
+++ src/pow.core.js
@@ -25,7 +25,7 @@ pow.module('core', function() {
 
 	pow.on = {}
 	pow.on.load = new pow.signal()  // After DOM loading completes
-	pow.on.start = new pow.signal() // After pow loading completes.
+	pow.on.start = new pow.signal() // After pow has initialized.
 	// pow.on.cleanup -- for cleaning the dom prior to saves?
 
 	pow.Animation = function(duration, args) {
